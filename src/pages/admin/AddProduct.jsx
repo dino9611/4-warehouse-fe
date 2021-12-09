7946195d04fc6c8dbfda20c85f104496616043a3
maulanas13 <maulana.septanto@hotmail.com>
2021-12-09 18:47:30 +0700

    [WJ-20] Add function & logic to detect user input & store the input, so can send it to BE database

--- src/pages/admin/AddProduct.jsx
+++ src/pages/admin/AddProduct.jsx
@@ -1,7 +1,106 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import "./styles/AddProduct.css"
+import {API_URL} from "../../constants/api";
+import thousandSeparator from '../../helpers/ThousandSeparator';
 
 function AdminAddProduct() {
+    const [role, setRole] = useState("superAdmin"); // Hanya untuk testing
 
+    const [addProdInput, setAddProdInput] = useState({
+        image: "",
+        prod_name: "",
+        prod_category: 0,
+        prod_weight: "",
+        prod_price: "",
+        prod_cost: "",
+        prod_stock: "",
+        prod_desc: ""
+      });
+    console.log(addProdInput);
+
+    const [addWhStock, setAddWhStock] = useState({
+        wh_id_01: 1,
+        stock_01: "",
+        wh_id_02: 2,
+        stock_02: "",
+        wh_03_id: 3,
+        stock_03: ""
+    });
+    console.log(addWhStock);
+
+    let { 
+        image, 
+        prod_name, 
+        prod_category, 
+        prod_weight, 
+        prod_price, 
+        prod_cost, 
+        prod_stock, 
+        prod_desc 
+    } = addProdInput;
+
+    let {
+        wh_id_01,
+        stock_01,
+        wh_id_02,
+        stock_02,
+        wh_03_id,
+        stock_03
+    } = addWhStock;
+
+    let stockList = [stock_01, stock_02, stock_03];
+
+    const [category, setCategory] = useState([]);
+    const [warehouse, setWarehouse] = useState([]);
+
+    const fetchCategory = async () => {
+        try {
+            const res = await axios.get(`${API_URL}/product/category`);
+            setCategory(res.data);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const fetchWarehouse = async () => {
+        try {
+            const res = await axios.get(`${API_URL}/warehouse/list`);
+            setWarehouse(res.data);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    useEffect(() => {
+        fetchCategory();
+        fetchWarehouse();
+        console.log("Setelah useEffect", addWhStock);
+    }, []);
+
+    // HANDLER FUNCTIONS SECTION
+    const addProdStringHandler = (event) => {
+        setAddProdInput((prevState) => {
+            return { ...prevState, [event.target.name]: event.target.value };
+        });
+    };
+
+    const addProdNumberHandler = (event, cb) => {
+        cb((prevState) => {
+            return { ...prevState, [event.target.name]: parseInt(event.target.value) };
+        });
+    };
+
+    const noMinusHandler = (event, cb) => { // Biar input number tidak negatif (-)
+        let input = event.target.value;
+        if (input < 0) {
+            cb((prevState) => {
+                return { ...prevState, [event.target.name]: input * -1 };
+            });
+        } else {
+            return
+        }
+    };
     
     return (
         <div className="add-products-main-wrap">
@@ -30,53 +129,70 @@ function AdminAddProduct() {
                 <form id="add-prod-form" className="add-info-form-wrap">
                     <div className="add-info-form-item">
                         <div className="add-info-form-left">
-                            <label for="prod-name">Product Name</label>
+                            <label htmlFor="prod_name">Product Name</label>
                         </div>
                         <div className="add-info-form-right">
                             <input 
                                 type="text" 
-                                id="prod-name" 
-                                name="prod-name" 
+                                id="prod_name" 
+                                name="prod_name" 
+                                value={prod_name}
+                                onChange={(event) => addProdStringHandler(event)}
                                 placeholder="Example: Javara (Brand, if any) + Coconut Sugar (Name) + 250gr (Size)"
                             />
                         </div>
                     </div>
                     <div className="add-info-form-item">
                         <div className="add-info-form-left">
-                            <label for="prod-category">Category</label>
+                            <label htmlFor="prod_category">Category</label>
                         </div>
                         <div className="add-info-form-right">
-                            <select name="prod-category" id="prod-category">
-                                <option value="" selected disabled hidden>Choose here</option>
-                                <option value="Kopi">Kopi</option>
-                                <option value="Coklat">Coklat</option>
-                                <option value="Susu">Susu</option>
+                            <select 
+                                id="prod_category"
+                                name="prod_category" 
+                                defaultValue={prod_category}
+                                onChange={(event) => addProdNumberHandler(event)}
+                                style={{textTransform: "capitalize"}}
+                            >
+                                <option value={0} disabled hidden>Select here</option>
+                                {category.map((val) => (
+                                    <option value={val.id} key={`00${val.id}-${val.category}`} style={{textTransform: "capitalize"}}>
+                                        {val.category}
+                                    </option>
+                                ))}
                             </select>
                         </div>
                     </div>
                     <div className="add-info-form-item">
                         <div className="add-info-form-left">
-                            <label for="prod-weight">Product Weight</label>
+                            <label htmlFor="prod_weight">Product Weight</label>
                         </div>
                         <div className="add-info-form-right">
                             <input 
-                                type="text" 
-                                id="prod-weight" 
-                                name="prod-weight" 
+                                type="number" 
+                                id="prod_weight" 
+                                name="prod_weight" 
+                                value={prod_weight}
+                                onChange={(event) => addProdNumberHandler(event, setAddProdInput)}
+                                onKeyUp={(event) => noMinusHandler(event, setAddProdInput)}
                                 placeholder="(base weight + packaging)"
+                                min="1"
                             />
                             <p>Gram (g)</p>
                         </div>
                     </div>
                     <div className="add-info-form-item">
                         <div className="add-info-form-left">
-                            <label for="prod-price">Product Price / Pcs</label>
+                            <label htmlFor="prod_price">Product Price / Pcs</label>
                         </div>
                         <div className="add-info-form-right">
                             <input 
                                 type="number" 
-                                id="prod-price" 
-                                name="prod-price" 
+                                id="prod_price" 
+                                name="prod_price" 
+                                value={prod_price}
+                                onChange={(event) => addProdNumberHandler(event, setAddProdInput)}
+                                onKeyUp={(event) => noMinusHandler(event, setAddProdInput)}
                                 placeholder="Input price (minimum: 1)"
                                 min="1"
                             />
@@ -85,32 +201,76 @@ function AdminAddProduct() {
                     </div>
                     <div className="add-info-form-item">
                         <div className="add-info-form-left">
-                            <label for="prod-stock">Stock (per Warehouse)</label>
+                            <label htmlFor="prod_cost">Product Cost / Pcs</label>
                         </div>
                         <div className="add-info-form-right">
                             <input 
                                 type="number" 
-                                id="prod-stock" 
-                                name="prod-stock" 
-                                placeholder="Input stock (minimum: 0)"
-                                min="0"
-                                disabled
+                                id="prod_cost" 
+                                name="prod_cost" 
+                                value={prod_cost}
+                                onChange={(event) => addProdNumberHandler(event, setAddProdInput)}
+                                onKeyUp={(event) => noMinusHandler(event, setAddProdInput)}
+                                placeholder="Product COGS (minimum: 1)"
+                                min="1"
                             />
-                            <p>*Only super admin can fill</p>
+                            <p>in Rupiah (Rp)</p>
                         </div>
                     </div>
+                    {/* <div className="add-info-form-item"> */}
+                        {warehouse.map((val, index) => (
+                            <div className="add-info-form-item" key={`Gudang-${val.id}`}>
+                                <div className="add-info-form-left">
+                                    <label htmlFor={`stock_0${val.id}`}>Stock {val.name}</label>
+                                </div>
+                                <div className="add-info-form-right">
+                                    <input 
+                                        type="number" 
+                                        id={`stock_0${val.id}`}
+                                        name={`stock_0${val.id}`}
+                                        value={stockList[index]}
+                                        onChange={(event) => addProdNumberHandler(event, setAddWhStock)}
+                                        onKeyUp={(event) => noMinusHandler(event, setAddWhStock)}
+                                        placeholder="Input stock (minimum: 0)"
+                                        min="0"
+                                        disabled={role === "admin"}
+                                    />
+                                    <p>*Only super admin can fill</p>
+                                </div>
+                            </div>
+                        ))}
+                        {/* <div className="add-info-form-left">
+                            <label htmlFor="prod_stock">Stock (per Warehouse)</label>
+                        </div>
+                        <div className="add-info-form-right">
+                            <input 
+                                type="number" 
+                                id="prod_stock" 
+                                name="prod_stock" 
+                                value={prod_stock}
+                                onChange={(event) => addProdNumberHandler(event)}
+                                onKeyUp={(event) => noMinusHandler(event)}
+                                placeholder="Input stock (minimum: 0)"
+                                min="0"
+                                disabled={role === "admin"}
+                            />
+                            <p>*Only super admin can fill</p>
+                        </div> */}
+                    {/* </div> */}
                 </form>
                 <div className="add-desc-form-wrap">
                     <div className="add-desc-form-item">
                         <div className="add-desc-form-left">
-                            <label for="prod-desc">Product Description</label>
+                            <label htmlFor="prod_desc">Product Description</label>
                         </div>
                         <div className="add-desc-form-right">
                             <textarea 
                                 type="text" 
                                 rows="8"
                                 cols="100"
-                                name="prod-desc" 
+                                name="prod_desc" 
+                                value={prod_desc}
+                                onChange={(event) => addProdStringHandler(event)}
                                 placeholder="High quality Indonesia cacao beans, harvested from the best source possible, offering rich chocolaty taste which will indulge you in satisfaction."
                             >
                             </textarea>
