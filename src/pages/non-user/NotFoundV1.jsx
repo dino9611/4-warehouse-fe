219cb78f408f60a8ccc7af9ef60907453eae5f99
maulanas13 <maulana.septanto@hotmail.com>
2022-01-08 22:00:42 +0700

    [WJ-Adm-Bug] Edit 404 page

--- src/pages/non-user/NotFoundV1.jsx
+++ src/pages/non-user/NotFoundV1.jsx
@@ -1,24 +1,32 @@
 import "./styles/NotFoundV1.css";
-import Admin404Img from "../../assets/visuals/Admin_Not_Found_01.jpeg";
-import {Link} from "react-router-dom";
+import Admin404Img from "../../assets/components/404.svg";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import AdmBtnPrimary from "../../components/admin/AdmBtnPrimary";
 
 function AdminNotFound() {
-    
+    const getRoleId = useSelector((state) => state.auth.role_id);
+
+    let history = useHistory();
+
+    //* Utk kembali ke homepage
+    const backToHome = () => (getRoleId === 1 || getRoleId === 2) ? history.push("/admin/dashboard") : history.push("/");
     
     return (
         <div className="adm-404-main-wrap">
             <div className="adm-404-sub-wrap">
-                <div className="adm-404-heading-wrap">
-                    <h2>404 Page Not Found!</h2>
-                </div>
                 <div className="adm-404-img-wrap">
                     <img src={Admin404Img}  alt="Admin-Not-Found" />
                 </div>
+                <div className="adm-404-body-wrap">
+                    <h2>Are you lost?</h2>
+                    <h6>Looks like the page you are trying to visit doesn't exist.</h6>
+                    <h6>Please check the correct URL and try again.</h6>
+                </div>
                 <div className="adm-404-foot-wrap">
-                    <h2>
-                        Are you lost traveller??? {" "}
-                        <Link to="/" className="link-no-decoration adm-404-cstm-link">Click here to homepage</Link>
-                    </h2>
+                    <AdmBtnPrimary padding={"0.625rem 1rem"} onClick={backToHome}>
+                        {(getRoleId === 1 || getRoleId === 2) ? "Back to home" : "Back to homepage"}
+                    </AdmBtnPrimary>
                 </div>
             </div>
         </div>
