70d9ae6ecaa745e1e19766c5dc9d39daf66404ca
gangsararyop <86463063+gangsararyop@users.noreply.github.com>
2022-02-05 18:43:35 +0700

    WJ-responsive-gangsar resp product.jsx

---
+++ src/components/ModalSort.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import ReactDom from "react-dom";
+import ClickOutside from "./ClickOutside";
+import { useTransition, animated } from "react-spring";
+import "./styles/modal.css";
+
+function ModalSort({ open, close, children, classModal }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+  }, [open]);
+
+  // Transition modal
+
+  const transitionModal = useTransition(open, {
+    from: { transform: "translate(-50%, 100%)", top: "150%" },
+    enter: { transform: "translate(-50%, -50%)", top: "100%", left: "50%" },
+    leave: { transform: "translate(-50%, 0%)" },
+  });
+
+  ClickOutside(ref, close);
+
+  if (!open) return null;
+
+  return ReactDom.createPortal(
+    <>
+      <div>
+        <div className="modal-overlay-style"></div>
+        {transitionModal((style, item) =>
+          item ? (
+            <animated.div
+              style={style}
+              ref={ref}
+              className={`modal-style ${classModal}`}
+            >
+              {children}
+            </animated.div>
+          ) : null
+        )}
+      </div>
+      )
+    </>,
+    document.getElementById("portal")
+  );
+}
+
+export default ModalSort;
